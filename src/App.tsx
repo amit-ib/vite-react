74c5e83dc86c40c10b89eba264b3f20ada33bc38
amit-ib <amit.verma@infobeans.com>
2025-12-05 12:31:21 +0530

    Testing1

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,8 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+
+      
       <p>Click on the Vite and React logos to learn more</p>
     </>
   );
