da743b6c9d76a41ae8348153109f4bcd0637d910
amit-ib <amit.verma@infobeans.com>
2025-12-11 17:25:53 +0530

    test12

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,11 @@ function App() {
         <>
             <div>
                 <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="logo" />
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
                 </a>
                 <a href="https://react.dev" target="_blank">
                     <img
